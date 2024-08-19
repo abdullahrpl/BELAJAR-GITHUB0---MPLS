f9bbd7aed54bcbee364bd2684ba1ec1d481c7cc1
Abdullah <abdullahibniwan@gmail.com>
2024-08-19 11:37:15 +0700

    learn variable and operators

---
+++ learning/js/operator.js
@@ -0,0 +1,46 @@
+/*
+OPERATORS
+1. Arithematical
+2. Logical
+3. Assignment
+*/
+
+let angka = 5;
+angka = angka  ** 3;
+if (angka == 125) {
+    angka = "a";
+}
+
+let number1 = 5;
+let number2 = 10;
+let result1 = number1 < number2; //true
+let result2 = number1 > number2; //false
+
+let negation = !result1;
+console.log("Before: " + result1); 
+console.log("After: " + negation);
+
+/*
+ARITHMETICAL OPERATORS
+1. Angka
+2. Operasi aritmatika
+Operator:
+1. + (Addition)
+2. - (Subtract)
+3. * (Multiply)
+4. / (Divide)
+6. -- (Multiply)
+7. ** (Decrement)
+8. ++ (Increment)
+9. ! tdk sama dgn
+10. == sama dgn
+11. && AND
+13. || OR
+*/
+
+/*
+OPERAND
+*/
+
+// const angka1 = 10;
+// angka1 = angka1 + 10;
---
+++ learning/js/variable.js
@@ -0,0 +1,20 @@
+// komentar hanya 1 baris
+/*
+    komentar 
+    bisa
+    lebih
+    dari
+    1 baris
+*/
+
+let abc = "ABC";
+abc = 123;
+
+// 1. Bisa Diubah
+// 2. Harus dideklarasi dan tidak bisa dideklarasi ulang
+
+const angka = 123;
+const angka1 = angka * 2;
+// 1. Konstanta atau tidak bisa diubah valenya
+// 2. Harus dideklarasi dan tidak bisa dideklarasi ulang
+console.log(angka1 + angka);
